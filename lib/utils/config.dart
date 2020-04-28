94e0bed02c04282d4d58c464df415869aa61f7d8
Jan Czizikow <jan@gohidoc.com>
2020-04-28 02:31:42 +0200

    chore: switch the prod backend url

--- lib/utils/config.dart
+++ lib/utils/config.dart
@@ -1,9 +1,9 @@
-import 'package:flutter/foundation.dart';
 import 'package:dio/dio.dart';
+import 'package:flutter/foundation.dart';
 
 final BaseOptions dioBaseOptions = BaseOptions(
   baseUrl: kReleaseMode
-      ? 'https://sliceit.herokuapp.com/api/v1'
+      ? 'https://www.sliceitapp.com/api/v1'
       : 'http://192.168.178.111:3000/api/v1',
   headers: {
     'Accept': 'application/json',
